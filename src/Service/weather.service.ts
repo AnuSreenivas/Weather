b0af567c69c6b2c08081c537dbbba0715ed02ec7
anumolpr <anumol@initsolutions.in>
2021-11-22 13:36:58 +0530

    second

--- src/Service/weather.service.ts
+++ src/Service/weather.service.ts
@@ -15,7 +15,7 @@ export class WeatherService {
     headers.append('Content-Type', 'application/json');
     let options = { headers: headers };
 
-    return this.http.get("http://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=metric&appid=d5cd58bf6ad991168af99f4db6805328",options).pipe<any>(map(res => res));
+    return this.http.get("https://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=metric&appid=d5cd58bf6ad991168af99f4db6805328",options).pipe<any>(map(res => res));
   // api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
   // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
   }
